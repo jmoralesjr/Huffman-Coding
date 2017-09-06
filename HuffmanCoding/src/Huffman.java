6e7d2e3c621ab2f54b76885b0502ec8ed5d942a6
Juan Morales <jmorales@1895@gmail.com>
2017-09-05 20:44:30 -0400

    Added code for methods processCodes, Huffman, treeHeight, and buildAll

--- HuffmanCoding/src/Huffman.java
+++ HuffmanCoding/src/Huffman.java
@@ -11,7 +11,35 @@ public class Huffman {
 	Queue<BinaryTree<Float>> trees;
 	ArrayList<BinaryTree<Float>> locations;
 	
-	public Huffman(char[] symbols, float[] probs){
+	public Huffman(char[] symbols, float[] probs) throws Exception{
+		
+		this.symbols = symbols;
+		this.probs = probs;
+		locations = new ArrayList<BinaryTree<Float>>(symbols.length);
+	
+		if(symbols.length == 1){
+			BinaryTree<Float> leaf = new BinaryTree<Float>();
+			leaf.makeRoot(0f);
+			locations.add(leaf);
+			huffman = new BinaryTree<Float>();
+			huffman.makeRoot(probs[0]);
+			huffman.addLeft(leaf);
+			leaf.parent = huffman;
+			return;
+		}
+		
+		buildLeaves();
+		
+		BinaryTree<Float> leaf1 = leaves.remove();
+		BinaryTree<Float> leaf2 = leaves.remove();
+		
+		buildTree(leaf1, leaf2);
+		
+		buildAll();
+		
+		huffman = trees.remove();
+		
+		processCodes();
 		
 	}
 	
@@ -22,14 +50,33 @@ public class Huffman {
 
 	public String getCode(char symbol){
 		return null;
-		
 	}
 	
 	
 	public void processCodes(){
+		int height = treeHeight(huffman);
+		char[] codeString = new char[height];
 		
+		codes = new String[locations.size()];
 		
-		
+		for(int i = 0; i < locations.size(); i++){
+			int index = height;
+			BinaryTree<Float> node = locations.get(i);
+			BinaryTree<Float> parent = node.parent;
+			
+			while(parent != null){
+				index--;
+				if(parent.left == node){
+					codeString[index] = '0';
+				}else{
+					codeString[index] = '1';
+				}
+				node = parent;
+				parent = node.parent;
+			}
+			
+			codes[i] = new String(codeString, index, height - index);
+		}
 	}
 	
 	/*
@@ -86,13 +133,56 @@ public class Huffman {
 		trees.add(root);
 	}
 	
-	public void BuildAll(){
+	/*
+	 * buildAll builds the entirety of the binary tree by finding the smallest the probability 
+	 * between leaves and trees and then combining them until a single binary tree is formed
+	 */
+	
+	public void buildAll() throws Exception{
+		
+		while(!leaves.isEmpty()){
+			
+			BinaryTree<Float> left = selectMin();
+			BinaryTree<Float> right = null;
+			
+			if(leaves.isEmpty()){
+				right = trees.remove();
+			}else{
+				right = selectMin();
+			}
+			
+			buildTree(left, right);
+		}
+		
+		while(!trees.isEmpty()){
+			if(trees.size() == 1){
+				return;
+			}
+			
+			BinaryTree<Float> left = trees.remove();
+			BinaryTree<Float> right = trees.remove();
+			
+			buildTree(left, right);
+		}
 		
 	}
 	
-	public void treeHeight(){
+	/*
+	 * treeHeight returns the height of a tree through recursion 
+	 */
+	
+	public int treeHeight(BinaryTree<Float> tree){
+		if(tree == null){
+			return -1;
+		}
 		
+		return Math.max(treeHeight(tree.left), treeHeight(tree.right)) + 1;
 	}
+	
+	/*
+	 * getProb returns the probability attached to the leaf node that it
+	 * was called on
+	 */
 	
 	public float getProb(BinaryTree<Float> tree){
 		if(tree.left == null && tree.right == null){
